3cdf2413a89d40380ecee1734ce9cd1bc36f079c
Amol Jagtap <amoljagtap17@gmail.com>
2020-01-05 20:06:31 +0530

    RRTS: Redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { App } from './components/App'
+import { reducers } from './reducers/index'
 
-interface AppProps {
-  color?: string
-}
+const store = createStore(reducers, applyMiddleware(thunk))
 
-class App extends Component<AppProps> {
-  state = {
-    counter: 0
-  }
-
-  onIncrement = (): void => {
-    this.setState({
-      counter: this.state.counter + 1
-    })
-  }
-
-  onDecrement = (): void => {
-    this.setState({
-      counter: this.state.counter - 1
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App color="red" />, document.querySelector('#root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+)
